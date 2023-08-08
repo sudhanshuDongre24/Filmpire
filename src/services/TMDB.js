b6ddf7b65713edb2cfbc783820a1fc94f60fef5c
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-08-08 19:03:37 +0530

    Movie and Genres fetching completed

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,12 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const tmdbApiKey = import.meta.env.VITE_TMDB_KEY;
 
+const page = 1;
 // '/language=en-US&page=1';
 
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
   endpoints: (builder) => ({
+    //*Get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
+
     // *Get Movies by [Type]
     getMovies: builder.query({
       query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
@@ -14,4 +20,4 @@ export const tmdbApi = createApi({
   }),
 });
 
-export const { useGetMoviesQuery } = tmdbApi;
+export const { useGetGenresQuery, useGetMoviesQuery } = tmdbApi;
